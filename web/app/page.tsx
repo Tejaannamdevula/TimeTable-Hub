32d101460915e136bceaf8dc699a928b9edb6c09
Teja Annamdevula <tejaannamdevula@gmail.com>
2024-08-11 16:01:28 +0530

    feat:built basic timetable

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,184 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+import { TimeTable } from "@/components/TimeTable";
+import { start } from "repl";
+export enum DayName {
+  MON = "MON",
+  TUE = "TUE",
+  WED = "WED",
+  THU = "THU",
+  FRI = "FRI",
+  SAT = "SAT",
+}
+const data = {
+  section: {
+    secName: "4D",
+    roomNo: 504,
+  },
+  instructors: [
+    {
+      name: "Mrs.S.Anitha",
+      subject: "Computing Ethics",
+    },
+    {
+      name: "Mrs.V Anusha",
+      subject: "Cryptography and Network Security",
+    },
+    {
+      name: "Ms.Lalitha",
+      subject: "Big Data and Analytics",
+    },
+    {
+      name: "Ms.Suvarna",
+      subject: "NPTEL",
+    },
+    {
+      name: "Dr. N. Sameera",
+      subject: "Cloud Computing",
+    },
+    {
+      name: "Dr. S.V. Phani Kumar",
+      subject: "Deep Learning",
+    },
+  ],
+  days: [
+    {
+      dayName: DayName.MON,
+      periods: [
+        { subject: "CNS", isLab: false, isBreak: false },
+        { subject: "DL", isLab: false, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "CE", isLab: false, isBreak: false },
+        { subject: "CC (L)", isLab: true, isBreak: false },
+        { subject: "CC (L)", isLab: true, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "BDA (L)", isLab: true, isBreak: false },
+        { subject: "BDA (L)", isLab: true, isBreak: false },
+      ],
+    },
+    {
+      dayName: DayName.TUE,
+      periods: [
+        {
+          subject: "BDA",
+          specialRoom: "307",
+          isLab: false,
+          isBreak: false,
+        },
+        {
+          subject: "NPTEL",
+          specialRoom: "307",
+          isLab: false,
+          isBreak: false,
+        },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "DL", isLab: false, isBreak: false },
+        { subject: "CC", isLab: false, isBreak: false },
+        { subject: "BDA", isLab: false, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "FSD", isLab: true, isBreak: false },
+      ],
+    },
+    {
+      dayName: DayName.WED,
+      periods: [
+        { subject: "DL (L)", isLab: true, isBreak: false },
+        { subject: "DL (L)", isLab: true, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "CNS", isLab: false, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        {
+          subject: "NPTEL",
+          specialRoom: "308",
+          isLab: false,
+          isBreak: false,
+        },
+        { subject: "CE", specialRoom: "308", isLab: false, isBreak: false },
+      ],
+    },
+    {
+      dayName: DayName.THU,
+      periods: [
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "CNS (L)", isLab: true, isBreak: false },
+        { subject: "CNS (L)", isLab: true, isBreak: false },
+        { subject: "BDA", isLab: false, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "CC", specialRoom: "307", isLab: false, isBreak: false },
+        { subject: "CE", specialRoom: "307", isLab: false, isBreak: false },
+      ],
+    },
+    {
+      dayName: DayName.FRI,
+      periods: [
+        { subject: "DL (L)", isLab: true, isBreak: false },
+        { subject: "DL (L)", isLab: true, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "FSD", isLab: true, isBreak: false },
+        { subject: "CC", isLab: false, isBreak: false },
+        { subject: "Break", isLab: false, isBreak: true },
+        null,
+        null,
+      ],
+    },
+    {
+      dayName: DayName.SAT,
+      periods: [null, null, null, null, null, null, null, null, null],
+    },
+  ],
+  timeslots: [
+    {
+      startTime: "08:15",
+      endTime: "9:10",
+    },
+    {
+      startTime: "09:10",
+      endTime: "10:05",
+    },
+    {
+      startTime: "10:05",
+      endTime: "10:20",
+    },
+    {
+      startTime: "10:20",
+      endTime: "11:15",
+    },
+    {
+      startTime: "11:15",
+      endTime: "12:10",
+    },
+    {
+      startTime: "12:10",
+      endTime: "01:05",
+    },
+    {
+      startTime: "01:05",
+      endTime: "02:00",
+    },
+    {
+      startTime: "02:00",
+      endTime: "02:55",
+    },
+    {
+      startTime: "02:55",
+      endTime: "03:50",
+    },
+  ],
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      <div className="flex  flex-col items-center justify-around  space-y-4">
         <Button>TimeTable App</Button>
+        <TimeTable data={data}></TimeTable>
       </div>
     </main>
   );
--- web/components/TimeTable.tsx
+++ web/components/TimeTable.tsx
@@ -1,8 +1,14 @@
-type TimeTabledata = {
+"use client";
+
+type TimeTableData = {
   section: Section;
   instructors: Instructor[];
   days: Day[];
   timeslots: TimeSlots;
+};
+
+type TimeTableProps = {
+  data: TimeTableData;
 };
 
 type Section = {
@@ -31,7 +37,7 @@ enum DayName {
 }
 type Day = {
   dayName: DayName;
-  periods: Period[];
+  periods: (Period | null)[];
 };
 
 type Period = {
@@ -40,3 +46,93 @@ type Period = {
   isLab: boolean;
   isBreak: boolean;
 };
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const SlotFunction = ({
+  data,
+  periodNo,
+}: {
+  data: Slots;
+  periodNo: number;
+}): JSX.Element => {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="w-full">{periodNo}</div>
+      <div className="w-full">
+        {data.startTime} - {data.endTime}
+      </div>
+    </div>
+  );
+};
+
+const DayClassFunction = ({ data }: { data: Day }): JSX.Element => {
+  return (
+    <>
+      <TableCell>{data.dayName}</TableCell>
+      {data.periods.map((period, index) => (
+        <TableCell key={index}>{period ? period.subject : " "}</TableCell>
+      ))}
+    </>
+  );
+};
+const InstructorFunction = ({ data }: { data: Instructor }): JSX.Element => {
+  return (
+    <TableRow>
+      <TableCell className=" text-center">{data.name}</TableCell>
+      <TableCell className=" text-center">{data.subject}</TableCell>
+    </TableRow>
+  );
+};
+export function TimeTable({ data }: TimeTableProps): JSX.Element {
+  return (
+    <div>
+      <div className="flex w-full  justify-center items-center mb-4 space-x-5 ">
+        <div className="text-lg font-bold">{data.section.secName}</div>
+        <div className="text-lg">{data.section.roomNo}</div>
+      </div>
+      <Table className=" w-[1000px]">
+        <TableHeader>
+          <TableRow className="w-full">
+            <TableHead className="w-[100px] text-center">DAY/HOUR</TableHead>
+
+            {data.timeslots.map((item, index) => (
+              <TableHead className="w-[100px] text-center" key={index}>
+                <SlotFunction data={item} periodNo={index + 1} />
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {data.days.map((day, index) => (
+            <TableRow key={index}>
+              <DayClassFunction data={day}></DayClassFunction>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <div>
+        <Table>
+          <TableBody>
+            {data.instructors.map((instructor, index) => (
+              <InstructorFunction
+                key={index}
+                data={instructor}
+              ></InstructorFunction>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
